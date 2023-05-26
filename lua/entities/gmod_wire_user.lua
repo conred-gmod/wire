2134a3801b191b6aeeee7c2a22c44eb19a243af8
Flarky55 <no-reply@conred.ru>
2023-05-26 17:52:56 +0600

    Changed Wire User use function to Entity.Input

--- lua/entities/gmod_wire_user.lua
+++ lua/entities/gmod_wire_user.lua
@@ -37,11 +37,7 @@ function ENT:TriggerInput(iname, value)
 
 		if not hook.Run( "PlayerUse", ply, trace.Entity ) then return false end
 
-		if trace.Entity.Use then
-			trace.Entity:Use(ply,ply,USE_ON,0)
-		else
-			trace.Entity:Fire("use","1",0)
-		end
+		trace.Entity:Input("use", ply, self)
 	end
 end
 
