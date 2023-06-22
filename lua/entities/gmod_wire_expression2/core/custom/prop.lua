2a7450c7ceea019f89e0f320dcfde50947d18660
Flarky55 <no-reply@conred.ru>
2023-06-23 00:48:09 +0600

    Changed Expression2 function entity:use()

--- lua/entities/gmod_wire_expression2/core/custom/prop.lua
+++ lua/entities/gmod_wire_expression2/core/custom/prop.lua
@@ -285,11 +285,8 @@ e2function void entity:use()
 	if not IsValid(ply) then return end -- if the owner isn't connected to the server, do nothing
 
 	if not hook.Run( "PlayerUse", ply, this ) then return end
-	if this.Use then
-		this:Use(ply,ply,USE_ON,0)
-	else
-		this:Fire("use","1",0)
-	end
+
+	this:Input("use", ply, self.entity)
 end
 
 __e2setcost(30)
